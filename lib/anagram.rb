ac879d1b7251a5d933916a34283f8180b19d2325
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-01 02:30:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,19 @@ class Anagram
   end
 
   def match(anagrams)
-    binding.pry
+    @word = @word.split(//)
+    @word.sort!
+
+    @split_anagrams = []
+
+    @anagrams = anagrams
+    @anagrams.each { |word| @split_anagrams << word.split(//) }
+
+    @split_anagrams.each { |word| word.sort! }
+
+    @split_anagrams.find{|word| word == @word }
   end
 end
-
-x = Anagram.new("listen")
-x.match(%w(hello world zombies pants dipper))
 
 #@word = @word.split(//)
 #@word.sort!
