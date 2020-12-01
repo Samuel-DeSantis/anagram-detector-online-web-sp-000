0a3b93f4019ac32cbdf4b9650e70152d859e96e6
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-01 03:37:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,21 +9,6 @@ class Anagram
 
   def match(anagrams)
     @anagrams = anagrams
-    @anagrams.select.with_index { |word, index| @word.split(//).sort == word.split(//).sort }
+    @anagrams.select { |word| @word.split(//).sort == word.split(//).sort }
   end
 end
-
-# Word compares against anagram [ , ]
-# Word and Anagram needs split and sorted
-
-#@word = @word.split(//)
-#@word.sort!
-
-#@split_anagrams = []
-
-#@anagrams = anagrams
-#@anagrams.each { |word| @split_anagrams << word.split(//) }
-
-#@split_anagrams.each { |word| word.sort! }
-
-#@split_anagrams.find{|word| word == @word }
